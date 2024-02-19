cdda092ebd093d39580441ade5701c2380c4b37d
Ian McPhail <webguyian@gmail.com>
2024-02-18 19:37:22 -0500

    Create CountryList and CountryCard components

---
+++ src/components/CountryCard.vue
@@ -0,0 +1,34 @@
+<template>
+  <router-link
+    class="appearance-none w-[250px] bg-white border border-gray-200 shadow dark:bg-gray-700 dark:border-gray-600"
+    :to="{ name: ROUTES.detail.name, params: { id: country.cca3 } }"
+  >
+    <div
+      class="border-b border-gray-300 h-40 bg-no-repeat bg-cover bg-center dark:border-gray-700"
+      :style="{ backgroundImage: `url(${country.flags?.svg})` }"
+    ></div>
+    <div class="p-5 text-left">
+      <h2
+        class="mb-2 text-md font-bold tracking-tight text-gray-900 dark:text-white"
+      >
+        {{ country.name?.common }}
+      </h2>
+      <dl class="text-sm grid grid-cols-2">
+        <dt class="font-bold">Capital:</dt>
+        <dd>{{ country.capital?.[0] ?? 'N/A' }}</dd>
+        <dt class="font-bold">Region:</dt>
+        <dd>{{ country.region }}</dd>
+        <dt class="font-bold">Population:</dt>
+        <dd>{{ country.population?.toLocaleString('en-us') }}</dd>
+      </dl>
+    </div>
+  </router-link>
+</template>
+
+<script setup>
+import { ROUTES } from '../const';
+
+const props = defineProps({
+  country: { type: Object, required: true }
+});
+</script>
--- src/components/CountryList.vue
+++ src/components/CountryList.vue
@@ -1,5 +1,29 @@
 <template>
-  <div>Country List</div>
+  <div>
+    <div class="grid grid-cols-fluid justify-items-center gap-10 mb-8">
+      <CountryCard
+        v-for="country in countries"
+        :key="country.cca3"
+        :country="country"
+      />
+    </div>
+    <p v-if="!countryCount">No matching countries found.</p>
+    <button
+      v-if="showMore"
+      class="py-2.5 px-8 me-2 mb-2 text-base font-medium text-gray-900 focus:outline-none bg-white rounded border border-gray-200 hover:bg-gray-100 focus:ring-2 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-700"
+      type="button"
+      @click="onLoadMore"
+    >
+      Load more
+    </button>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { useRoute } from 'vue-router';
+import useCountries from '../composables/use-countries';
+import CountryCard from './CountryCard.vue';
+
+const route = useRoute();
+const { countries, countryCount, onLoadMore, showMore } = useCountries(route);
+</script>
